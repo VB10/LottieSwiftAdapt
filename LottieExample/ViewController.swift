38d37b187f45f30175dbf2fed9c4e01d03416aa6
vb <vbacik.10@gmail.com>
2020-03-21 00:52:20 +0300

    Initial Commit

---
+++ LottieExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LottieExample
+//
+//  Created by Veli on 21.03.2020.
+//  Copyright © 2020 Veli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
